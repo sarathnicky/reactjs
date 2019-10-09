d744f0b176761f974ac64bab5604fa8da526e99d
sarathnicky <sarath.nicky0626@gmail.com>
2019-10-09 12:07:39 +0530

    Add files via upload

---
+++ precache-manifest.42b030cd4c4bc438ed25d80377144564.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f828b514d77a7d259d777f644cd58fde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cab3fde1627e92e35bc",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "31b29b26bc979ad0940b",
+    "url": "/static/js/2.1e8b7724.chunk.js"
+  },
+  {
+    "revision": "4cab3fde1627e92e35bc",
+    "url": "/static/js/main.86630b6e.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
